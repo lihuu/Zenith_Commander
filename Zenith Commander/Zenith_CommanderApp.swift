8b609bb69e5d2618d177ec3c1df577a4877fff73
lihuu <lihu1449488533@outlook.com>
2025-11-27 13:18:28 +0800

    Initial Commit

---
+++ Zenith Commander/Zenith_CommanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zenith_CommanderApp.swift
+//  Zenith Commander
+//
+//  Created by Hu Li on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Zenith_CommanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Zenith CommanderTests/Zenith_CommanderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zenith_CommanderTests.swift
+//  Zenith CommanderTests
+//
+//  Created by Hu Li on 11/27/25.
+//
+
+import Testing
+@testable import Zenith_Commander
+
+struct Zenith_CommanderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
