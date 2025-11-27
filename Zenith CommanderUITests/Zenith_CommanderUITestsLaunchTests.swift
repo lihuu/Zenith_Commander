13939a2f295ed8a64db94ac4ecb400fbed2cfff8
lihuu <lihu1449488533@outlook.com>
2025-11-27 13:18:28 +0800

    Initial Commit

---
+++ Zenith CommanderUITests/Zenith_CommanderUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Zenith_CommanderUITestsLaunchTests.swift
+//  Zenith CommanderUITests
+//
+//  Created by Hu Li on 11/27/25.
+//
+
+import XCTest
+
+final class Zenith_CommanderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
